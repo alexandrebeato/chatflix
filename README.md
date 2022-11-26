e2037f46719552bc0cc4d285962fb5953cd432e2
Alexandre Beato <alebeato9@gmail.com>
2022-11-26 03:10:35 -0300

    Changes

---
+++ README.md
@@ -0,0 +1,48 @@
+<p align="center">
+  <img src="logo.png" /> 
+</p>
+
+## Requirements
+- DotNet SDK
+- Docker
+
+## Getting started
+
+* Go to ```server``` folder.
+* Run the command ```docker-compose up -d```.
+* Now go to ```server/src/Stock.Service``` folder.
+* Run the command ```dotnet build run && dotnet run```
+* Now go to ```server/src/API``` folder.
+* Run the command ```dotnet build run && dotnet run```
+* Now go to the ```client``` folder and open how many ```chat.html``` you want to.
+
+## Give a Star! :star:
+If you liked the project or if Chatflix helped you, please give a star ;)
+
+## Technologies implemented:
+
+- ASP.NET 6.0
+- ASP.NET WebApi Core with JWT Bearer Authentication
+- Mongo database
+- .NET Core Native DI
+- AutoMapper
+- MediatR
+- Swagger UI
+- RabbitMQ
+- VueJS
+- Bootstrap
+- Axios
+
+## Architecture:
+
+- Full architecture with responsibility separation concerns, SOLID and Clean Code
+- Domain Driven Design (Layers and Domain Model Pattern)
+- Domain Events
+- Domain Notification
+- CQRS (Imediate Consistency)
+- Repository
+
+## About:
+The Equinox Project was developed by [Alexandre Beato](hhttps://www.instagram.com/alexandrebeato/) under the [MIT license](LICENSE).
+
+Made with ♥ by Ale
--- server/src/Stock.Service/Program.cs
+++ server/src/Stock.Service/Program.cs
@@ -1,13 +1,18 @@
 ﻿
 using Core.Domain.Interfaces;
 using Core.Domain.Models;
+using Domain.Messages;
+using Domain.Messages.Repository;
 using Infra.CrossCutting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 using Newtonsoft.Json;
 using Stock.Service.Interfaces;
 using Stock.Service.Services;
-using System.Net.Http;
 
 var builder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -17,18 +22,27 @@ var configuration = builder.Build();
 
 var serviceCollection = new ServiceCollection();
 serviceCollection.AddSingleton<IConfiguration>(configuration);
+
+// Mongo database
+serviceCollection.AddSingleton<IMongoClient>(mongo => new MongoClient(configuration["mongoConnection:server"]));
+BsonSerializer.RegisterSerializer(typeof(Guid), new GuidSerializer(BsonType.String));
+
 serviceCollection.AddSingleton<IStooqService, StooqService>();
 serviceCollection.AddSingleton<HttpClient>(new HttpClient());
 serviceCollection.RegisterServices();
 var serviceProvider = serviceCollection.BuildServiceProvider();
 var queueService = serviceProvider.GetService<IQueueService>();
 var stooqService = serviceProvider.GetService<IStooqService>();
+var messageRepository = serviceProvider.GetService<IMessageRepository>();
 
 if (queueService == null)
     throw new ArgumentNullException(nameof(queueService));
 
 if (stooqService == null)
     throw new ArgumentNullException(nameof(stooqService));
+
+if (messageRepository == null)
+    throw new ArgumentNullException(nameof(messageRepository));
 
 queueService.SubscribeAsync("stock", async (message) =>
 {
@@ -43,7 +57,20 @@ queueService.SubscribeAsync("stock", async (message) =>
         return;
 
     foreach (var stock in chatCommand.Arguments)
-        Console.WriteLine(JsonConvert.SerializeObject(await stooqService.GetStocks(stock)));
+    {
+        if (chatCommand.RoomId == null)
+            continue;
+
+        var stooqResult = await stooqService.GetStocks(stock);
+
+        if (stooqResult == null || stooqResult.Symbols == null || stooqResult.Symbols.Count() == 0)
+            continue;
+
+        Console.WriteLine(JsonConvert.SerializeObject(stooqResult));
+
+        foreach (var symbol in stooqResult.Symbols)
+            await messageRepository.Insert(Message.Factory.CreateInstance(Guid.NewGuid(), chatCommand.RoomId.Value, Guid.Empty, "STOCK BOT", $"{symbol.Symbol} quote is ${symbol.Close} per share", DateTime.UtcNow));
+    }
 
 });
 
